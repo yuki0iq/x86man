fc31834840386fbc8b0e26a27eee9229c563a383
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-11-20 03:54:30 +0300

    a bit of hacking with pandas and bs4

--- html-to-markdown.py
+++ html-to-markdown.py
@@ -2,7 +2,9 @@
 
 import bs4
 import cssutils
+import io
 import json
+import pandas
 import re
 import os
 import sys
@@ -26,13 +28,7 @@ for sel, sty in selectors.items():
     matches = soup.find_all("span", class_=sel[1:])
     for match in matches:
         assert sty.get("color") in ["#000", "#075fa8", "#6331af"]
-        if sty.get("text-decoration") == "underline":
-            match.wrap(soup.new_tag("u"))
-        if sty.get("font-style") == "italic":
-            match.wrap(soup.new_tag("i"))
-        if sty.get("font-weight") == "bold":
-            match.wrap(soup.new_tag("b"))
-        if sty.get("color") == "#075fa8":
+        if sty.get("color") == "#075fa8" and "https://" not in match.get_text():
             assert sty["font-size"] in ["8pt", "9pt", "9.5pt", "10pt", "11pt", "12pt"]
             if sty["font-size"] == "12pt":
                 global_name = match.get_text().replace('—', ' -- ')
@@ -44,6 +40,12 @@ for sel, sty in selectors.items():
                 match.name = "h2"
             if match.parent.name == "p":
                 match.parent.unwrap()
+        if sty.get("text-decoration") == "underline":
+            match.wrap(soup.new_tag("u"))
+        if sty.get("font-style") == "italic":
+            match.wrap(soup.new_tag("i"))
+        if sty.get("font-weight") == "bold":
+            match.wrap(soup.new_tag("b"))
         if sty.get("font-family") == "NeoSansIntel" and sty.get("font-size") == "8pt":
             for el in match.parents:
                 match.decompose()
@@ -86,7 +88,7 @@ for header in soup.find_all(string="Operation"):
             break
         pre.append(sib.extract())
     for el in list(pre.descendants):
-        if not el.get_text() and not el.name == "br":
+        if not el.get_text() and not el.name == "br" and not isinstance(el, bs4.Comment):
             el.decompose()
         if el.name == "p":
             el.insert_after(soup.new_tag("br"))
@@ -106,7 +108,9 @@ for header in soup.find_all(string="Operation"):
 soup.smooth()
 
 for header in soup.find_all(string=lambda x: "Intrinsic Equivalent" in x):
-    header = next(el for el in header.parents if el.name == "h1")
+    header = next((el for el in header.parents if el.name == "h1"), None)
+    if header is None:
+        continue
     pre = soup.new_tag("pre")
     for sib in list(header.next_siblings):
         if sib.name == "h1":
@@ -119,7 +123,13 @@ for header in soup.find_all(string=lambda x: "Intrinsic Equivalent" in x):
 
 soup.smooth()
 
-# with open("/tmp/a.html", "w") as file: print(soup, file=file)
+def is_table_well(soup):
+    count = set()
+    for row in soup.find_all("tr"):
+        count.add(len(row.find_all("th")) + len(row.find_all("td")))
+    return len(count) == 1
+
+with open("/tmp/a.html", "w") as file: print(soup, file=file)
 
 def convert(soup):
     def batch(children):
@@ -162,17 +172,43 @@ def convert(soup):
         yield "*"
 
     elif soup.name == "i":
-        yield "__"
+        yield "*"
         yield from batch(soup.contents)
-        yield "__"
+        yield "*"
 
     elif soup.name == "table":
         yield "\n\n"
-        yield from batch(soup.contents)
-        yield "\n"
+
+        if is_table_well(soup):
+            yield from batch(soup.contents)
+        else:
+            df = pandas.read_html(io.StringIO(str(soup)))[0]
+            table = df.transpose().to_dict()
+
+            for i, col in enumerate(df.columns):
+                if i == 0:
+                    yield "[[ "
+                else:
+                    yield ":[ "
+                yield "*"
+                yield str(col)
+                yield "*\n"
+
+            for _, line in sorted(table.items()):
+                for j, col in enumerate(df.columns):
+                    if j == 0:
+                        yield "|  "
+                    else:
+                        yield ":  "
+                    yield str(line[col]).replace('\n', ' ').strip()
+                    yield "\n"
 
     elif soup.name in ["td", "th"]:
         text = soup.get_text().strip()
+        colspan = 1
+        if 'colspan' in soup.attrs:
+            colspan = int(soup['colspan'])
+
         if soup.name == "th":
             if not soup.previous_sibling:
                 yield "[[ "
@@ -180,6 +216,8 @@ def convert(soup):
                 yield ":< "
             else:
                 yield ":[ "
+
+            yield from ('\n:[ ' for _ in range(1, colspan))
             yield "*"
         else:
             yield "|  " if not soup.previous_sibling else ":  "
@@ -195,12 +233,14 @@ def convert(soup):
 
         if soup.name == "th":
             yield "*"
+        else:
+            yield from ('\n:  ' for _ in range(1, colspan))
         yield "\n"
 
     else:
         yield from batch(soup.contents)
 
-name = outf[outf.rfind("/") + 1:outf.rfind(".")]
+name = outf[outf.rfind("/") + 1:outf.rfind(".")].replace('+', '_').replace(',', '_')
 md = f'''{name}(7intel) "Intel Software Developer's manual" "Intel x86 ISA reference"
 
 {''.join(convert(soup.find("body")))}
